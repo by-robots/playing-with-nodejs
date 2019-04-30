86c3672057c63a89d01de9792dfae82b65223c09
Dan Gates <skynet@by-robots.com>
2019-04-30 19:47:00 +0100

    Use an arrow function to start the server, mostly because it looks tidier to me.

--- server.js
+++ server.js
@@ -7,23 +7,14 @@ const url = require('url')
  * @callback route Callback to handle the app's routing.
  */
 function start (route) {
-  /**
-   * Handles an HTTP request.
-   *
-   * @param {Object} request  The request's details.
-   * @param {Object} response The response object.
-   */
-  function handleRequest (request, response) {
+  http.createServer((request, response) => {
     const pathname = url.parse(request.url).pathname
     route(pathname)
 
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.write('Hello World')
     response.end()
-  }
-
-  // Create the server and start listening for requests.
-  http.createServer(handleRequest).listen(1337)
+  }).listen(1337)
 }
 
 exports.start = start
