521969dfff15ba5488f67abbf3f29df690e331ac
Dan <skynet@by-robots.com>
2019-04-30 13:04:28 +0100

    Make the server a module that can be included by the index.js file.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http')
 
-http.createServer((request, response) => {
-  console.log('Request received.')
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.write('Hello World')
-  response.end()
-}).listen(1337)
+function start() {
+  http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.write('Hello World')
+    response.end()
+  }).listen(1337)
+}
 
-console.log('Server running.')
+exports.start = start
