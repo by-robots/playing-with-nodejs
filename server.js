fe176f9a3a669ded5e73e5cb0315874be3815e53
Dan <skynet@by-robots.com>
2019-04-30 13:11:13 +0100

    Get the request's path name.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const url  = require('url')
 
 /**
  * Handles an HTTP request.
@@ -7,6 +8,9 @@ const http = require('http')
  * @param {Object} response The response object.
  */
 function handleRequest(request, response) {
+  const pathname = url.parse(request.url).pathname
+  console.log(`Request for ${pathname} received.`)
+
   response.writeHead(200, { 'Content-Type': 'text/plain' })
   response.write('Hello World')
   response.end()
