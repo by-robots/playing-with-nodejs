c06d6e88a851bc13370626d1b00df0370a09f159
Dan <skynet@by-robots.com>
2019-04-30 13:07:10 +0100

    Re-factor the request handler into it's own function.

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const http = require('http')
 
+/**
+ * Handles an HTTP request.
+ *
+ * @param {Object} request  The request's details.
+ * @param {Object} response The response object.
+ */
+function handleRequest(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.write('Hello World')
+  response.end()
+}
+
+/**
+ * Start the HTTP server.
+ */
 function start() {
-  http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.write('Hello World')
-    response.end()
-  }).listen(1337)
+  http.createServer(handleRequest).listen(1337)
 }
 
 exports.start = start
