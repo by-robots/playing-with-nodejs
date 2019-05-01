741295dc901fe21ad8ddd861934d2db73c79d311
Dan Gates <skynet@by-robots.com>
2019-05-01 22:06:06 +0100

    Return after a request is successfully handle.
    Then I can remove an else statement and make the code a bit tidier.

--- router.js
+++ router.js
@@ -10,12 +10,13 @@ function route (handle, pathname, response) {
 
   if (typeof handle[pathname] === 'function') {
     handle[pathname](response)
-  } else {
-    console.log(`No request handler for ${pathname}`)
-    response.writeHead(404, { 'Content-Type': 'text/plain' })
-    response.write('404 Not Found')
-    response.end()
+    return
   }
+
+  console.log(`No request handler for ${pathname}`)
+  response.writeHead(404, { 'Content-Type': 'text/plain' })
+  response.write('404 Not Found')
+  response.end()
 }
 
 exports.route = route
