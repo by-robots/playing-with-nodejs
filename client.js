9021573d68c96d66910684c95e04c34c60f51734
Dan Gates <skynet@by-robots.com>
2019-05-10 21:27:43 +0100

    Re-factor to be asyncronous. Nice.

--- client.js
+++ client.js
@@ -1,31 +1,56 @@
 'use strict'
 
+// Require packages.
+const async = require('async')
 const request = require('request')
 
-request.get('http://localhost:8080/getUserName?id=1234', (err, res, body) => {
-  if (err) {
-    // TODO: Handle errors.
-  }
+/**
+ * Pseudo request a username.
+ *
+ * @param {Callable} callback Function to execute after the request is finished.
+ */
+const getUsername = function (callback) {
+  request.get('http://localhost:8080/getUserName?id=1234', (err, res, body) => {
+    if (err) {
+      // TODO: Handle errors.
+    }
 
-  const result = JSON.parse(body)
-  const name = result.value
+    const result = JSON.parse(body)
+    callback(err, result.value)
+  })
+}
 
-  // A second request called after the first has completed.
+/**
+ * A second request, called asyncronously, that pretends to get the status of a
+ * user.
+ *
+ * @param {Callable} callback Function to execute after the request is finished.
+ */
+const getUserStatus = function (callback) {
   request.get('http://localhost:8080/getUserStatus?id=1234', (err, res, body) => {
     if (err) {
       // TODO: Handle errors.
     }
 
     const result = JSON.parse(body)
-    const status = result.value
-
-    console.log(`The status of user ${name} is ${status}.`)
+    callback(err, result.value)
   })
+}
+
+/**
+ * Execute both functions in parallel before dealing with the responses.
+ */
+async.parallel([getUsername, getUserStatus], (err, results) => {
+  if (err) {
+    // TODO: You guessed it...
+  }
+
+  console.log(`The status of ${results[0]} is ${results[1]}.`)
 })
 
 /**
  * Example response of `time node client.js`
  *
- * The status of user 85 is 96.
- * node client.js  0.28s user 0.08s system 7% cpu 4.885 total
+ * The status of 81 is 89.
+ * node client.js  0.19s user 0.04s system 9% cpu 2.462 total
  */
