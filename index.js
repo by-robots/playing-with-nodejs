a3744d03fc7f3ffc117f3d4f3269915ae9e45acc
Dan Gates <skynet@by-robots.com>
2019-05-17 21:46:56 +0100

    Use the socket to send data back to the server and then have the server send the shared data back to all clients.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
 /**
- * Provide a root URI we can access.
+ * Provide a route we can access.
  */
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/html/index.html`)
@@ -14,17 +14,27 @@ app.get('/', (req, res) => {
  */
 io.on('connection', (socket) => {
   console.log('A new WebSocket connection has been established.')
+
+  /**
+   * Receive data from the client through the socket. When we receive a stock
+   * price share emit an event to all clients with the shared price.
+   */
+  socket.on('stock price share', (data) => {
+    io.emit('stock price shared', data)
+  })
 })
 
 /**
- * Randomly generate a stock price.
+ * Randomly generate a stock price every 1 - 2 seconds.
  */
 setInterval(() => {
   const stockprice = Math.floor(Math.random() * 1000)
   io.emit('stock price update', stockprice)
-}, Math.random() * (1000, 50) + 1000)
+}, Math.random() * (2000, 1000) + 1000)
 
 /**
- * All ears.
+ * Start the server and listen for connections.
  */
-http.listen(8000, () => { console.log('Listening on port 8000.') })
+http.listen(8000, () => {
+  console.log('Listening on port 8000.')
+})
--- readme.md
+++ readme.md
@@ -1,10 +1,8 @@
 # Playing With Node JS
 
-See branches for different excercises.
+``` sh
+node index.js
+```
 
-1. [Hello, world!](https://github.com/by-robots/playing-with-nodejs/tree/01-hello-world)
-2. [Basic CRUD](https://github.com/by-robots/playing-with-nodejs/tree/02-basic-crud)
-3. [TDD](https://github.com/by-robots/playing-with-nodejs/tree/03-tdd)
-4. [Custom Event Emitters](https://github.com/by-robots/playing-with-nodejs/tree/04-event-emitter)
-5. [Background Tasks](https://github.com/by-robots/playing-with-nodejs/tree/05-background-tasks)
-6. [Websockets](https://github.com/by-robots/playing-with-nodejs/tree/06-websockets)
+Then pop to `http://localhost:8000` in more than one browser (or tab) and start
+using the share button the send stock prices between clients.
